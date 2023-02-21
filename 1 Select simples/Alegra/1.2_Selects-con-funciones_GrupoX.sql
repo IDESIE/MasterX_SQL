36260da6f8c10babe63949186cf7d7a0928b09fc
JP <juanpablo.romero@idesie.com>
2023-02-21 08:46:27 +0100

    Update 1.2_Selects-con-funciones_GrupoX.sql

--- 1 Select simples/Alegra/1.2_Selects-con-funciones_GrupoX.sql
+++ 1 Select simples/Alegra/1.2_Selects-con-funciones_GrupoX.sql
@@ -24,8 +24,11 @@ del floorid 1. Redondeado a dos dígitos.
 */
 
 /* 4
-Listar el número de componentes que tienen indicado el espacio y el número de componentes total.
-del facility 1
+¿Cuántos componentes tienen espacio? ¿Cuántos componentes hay?
+En el facility 1. Ej.
+ConEspacio  Componentes
+----------------------------
+3500  4000
 */
 
 /* 5
